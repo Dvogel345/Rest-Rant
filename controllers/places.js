d33f306217fd83d37eb3dae3eaceceea6ecf65f2
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2022-01-03 20:09:21 -0500

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -52,4 +52,15 @@ router.get('*', (req, res) => {
     res.send('haloWorld')
 })
 
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if(isNaN(id)) {
+        res.render('error404')
+    } else if (!places[id]) {
+        res.render('error404')
+    } else {
+        res.render('places/show', {place: places[id] })
+    }
+})
+
 module.exports = router
